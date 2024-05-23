aaea649c14310eb9f19e7362dab7542496134902
Cyril Aguvasu <aguvasucyril@gmail.com>
2024-05-23 13:50:53 +0300

    fix: added 'web' middleware group to ensure session and CSRF protection

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,5 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use Aguva\Ussd\Controllers\TestController;
 
-Route::get('simulator', [TestController::class, 'showSimulator'])->name('test.show-simulator');
-Route::post('process-payload', [TestController::class, 'processPayload'])->name('test.process-payload');
+Route::middleware(['web'])->group(function () {
+    Route::get('simulator', [TestController::class, 'showSimulator'])->name('test.show-simulator');
+    Route::post('process-payload', [TestController::class, 'processPayload'])->name('test.process-payload');
+});
